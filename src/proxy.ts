c8b3e393882f78c42179f298fb67910ba4facd35
Kai Hentschel <kai@umami-creative.de>
2025-12-07 17:39:35 +0100

    ⬆️ nextjs@16.0.7

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,24 +1,24 @@
-import createMiddleware from "next-intl/middleware";
-import { ALL_LANGUAGES, DEFAULT_LANGUAGE } from "@/tolgee/shared";
+import createMiddleware from 'next-intl/middleware';
+import { ALL_LANGUAGES, DEFAULT_LANGUAGE } from '@/tolgee/shared';
 
-import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+import { NextRequest, NextResponse } from 'next/server';
+import { getSessionCookie } from 'better-auth/cookies';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const handleI18nRouting = createMiddleware({
     locales: ALL_LANGUAGES,
     defaultLocale: DEFAULT_LANGUAGE,
-    localePrefix: "always",
+    localePrefix: 'always',
     localeDetection: true,
   });
   const response = handleI18nRouting(request);
 
-  const [, locale, ...segments] = request.nextUrl.pathname.split("/");
+  const [, locale, ...segments] = request.nextUrl.pathname.split('/');
   const isLocale = ALL_LANGUAGES.includes(locale);
   if (!isLocale) return response;
 
   // public urls
-  const publicUrls = ["sign-in", "sign-up", "forgot-password", "reset-password", "verify-email"];
+  const publicUrls = ['sign-in', 'sign-up', 'forgot-password', 'reset-password', 'verify-email'];
   if (segments.some((segment) => publicUrls.includes(segment))) {
     return NextResponse.next();
   }
@@ -34,5 +34,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   // Skip all paths that should not be internationalized
-  matcher: ["/((?!api|_next|.*\\..*).*)"],
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
