efdc630e978c520a01a7d3acc07e1def62e4dd58
KaiSoellch <kai@umami-creative.de>
2026-05-25 17:29:53 +0200

    feat: add font size toggle

---
+++ apps/webapp/src/components/font-size-toggle.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { IconTextSize } from "@tabler/icons-react";
+import { useTranslate } from "@tolgee/react";
+
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+	FONT_SIZE_OPTIONS,
+	isFontSizePreference,
+	useFontSizePreference,
+} from "./font-size-preference";
+
+export function FontSizeToggle() {
+	const { t } = useTranslate();
+	const { fontSize, setFontSize } = useFontSizePreference();
+	const handleFontSizeChange = (value: string) => {
+		if (isFontSizePreference(value)) {
+			setFontSize(value);
+		}
+	};
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline" size="icon">
+					<IconTextSize aria-hidden="true" className="size-4" />
+					<span className="sr-only">{t("common:user.font-size", "Font size")}</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuRadioGroup value={fontSize} onValueChange={handleFontSizeChange}>
+					{FONT_SIZE_OPTIONS.map((option) => (
+						<DropdownMenuRadioItem key={option.value} value={option.value}>
+							{t(`common:${option.labelKey}`, option.label)}
+						</DropdownMenuRadioItem>
+					))}
+				</DropdownMenuRadioGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
